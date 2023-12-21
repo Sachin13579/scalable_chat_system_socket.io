415c194c6b07dc8ba8971aa00ae133d4590f0760
sachin singh <singh.sachin@resourcifi.com>
2023-12-22 01:33:50 +0530

    added / route for teting

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,8 +4,13 @@ import SocketService from "./services/socket";
 
 async function init() {
     const socketService = new SocketService()
-    const httpServer = http.createServer();
-    const PORT = process.env.port ? process.env.port : 8000;
+    const httpServer = http.createServer((req, res) => {
+        if (req.url == "/") {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            return res.end("hello world!");
+        }
+    });
+    const PORT = 8000;
     socketService.io.attach(httpServer);
     httpServer.listen(PORT, () => {
         console.log(`sever is listening on ${PORT}`)
